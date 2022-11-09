913135035a9078c965c084ed47b5725207cc4266
Benjamin Malengreau <benjamin.malengreau@imi-hydronic.com>
2022-11-09 19:49:57 +0100

    définition de MessageCard

--- app/src/main/java/be/blackgear/android101_sance2/ui/ConversationScreen.kt
+++ app/src/main/java/be/blackgear/android101_sance2/ui/ConversationScreen.kt
@@ -35,6 +35,18 @@ fun ConversationScreen(
     }
 }
 
+@Composable
+fun MessageCard(message: String, author: String, date: String, modifier: Modifier = Modifier) {
+}
+
+@Preview
+@Composable
+private fun MessageCardPreview() = MessageCard(
+    message = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    author = "Moi",
+    date = "9 novembre 2022 à 19h50",
+)
+
 @Preview
 @Composable
 private fun ConversationScreenPreview() = ConversationScreen(
