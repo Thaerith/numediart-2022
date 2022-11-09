4770bb98eb46c19b08cce2785522387785d7adef
Benjamin Malengreau <benjamin.malengreau@imi-hydronic.com>
2022-11-09 18:51:54 +0100

    ajout du lazycolumn
    lazycolumn = column permettant de charger/gérer un granch nombre
    d'éléments dans une liste.

--- app/src/main/java/be/blackgear/android101_sance2/ui/ConversationScreen.kt
+++ app/src/main/java/be/blackgear/android101_sance2/ui/ConversationScreen.kt
@@ -1,8 +1,10 @@
 package be.blackgear.android101_sance2.ui
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
+import androidx.compose.material.TextField
 import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
@@ -16,7 +18,7 @@ fun ConversationScreen() = Scaffold(
     }
 ) {
     Column {
-
+        LazyColumn {}
     }
 }
 
