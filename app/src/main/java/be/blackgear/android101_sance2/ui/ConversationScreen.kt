b2bd7e08d1a9f08867aeec05186f99d9e4fb45f7
Benjamin Malengreau <benjamin.malengreau@imi-hydronic.com>
2022-11-09 19:21:23 +0100

    ajout des modifiers du TextField
    1 pour fit la largeur
    1 pour ajouter les marges
    1 pour aligner dans la box

--- app/src/main/java/be/blackgear/android101_sance2/ui/ConversationScreen.kt
+++ app/src/main/java/be/blackgear/android101_sance2/ui/ConversationScreen.kt
@@ -1,17 +1,16 @@
 package be.blackgear.android101_sance2.ui
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
 import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun ConversationScreen(
@@ -26,7 +25,13 @@ fun ConversationScreen(
 ) {
     Box {
         LazyColumn(Modifier.fillMaxSize()) {}
-        TextField(value = message, onValueChange = onMessageChange)
+        TextField(
+            value = message, onValueChange = onMessageChange,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 16.dp, bottom = 16.dp, end = 16.dp)
+                .align(Alignment.BottomCenter)
+        )
     }
 }
 
