873d78f3e0399236fd9147a498730d2689a6544f
Benjamin Malengreau <benjamin.malengreau@imi-hydronic.com>
2022-11-09 18:43:57 +0100

    définition de ConversationScreen

---
+++ app/src/main/java/be/blackgear/android101_sance2/ui/ConversationScreen.kt
@@ -0,0 +1,12 @@
+package be.blackgear.android101_sance2.ui
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun ConversationScreen() {
+}
+
+@Preview
+@Composable
+private fun ConversationScreenPreview() = ConversationScreen()
