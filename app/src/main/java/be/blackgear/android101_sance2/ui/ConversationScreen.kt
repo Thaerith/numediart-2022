43ac888bfaa96dbe9080935679de1ba964a04cde
Benjamin Malengreau <benjamin.malengreau@imi-hydronic.com>
2022-11-09 19:19:40 +0100

    ajout du modifier pour fit tout l'écran

--- app/src/main/java/be/blackgear/android101_sance2/ui/ConversationScreen.kt
+++ app/src/main/java/be/blackgear/android101_sance2/ui/ConversationScreen.kt
@@ -2,12 +2,15 @@ package be.blackgear.android101_sance2.ui
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
 import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
@@ -22,7 +25,7 @@ fun ConversationScreen(
     }
 ) {
     Box {
-        LazyColumn {}
+        LazyColumn(Modifier.fillMaxSize()) {}
         TextField(value = message, onValueChange = onMessageChange)
     }
 }
